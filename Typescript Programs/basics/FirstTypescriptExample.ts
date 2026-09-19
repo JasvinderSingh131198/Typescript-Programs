245466c6354d829be457ef0c0d1a1da2c8def540
JASVINDER SINGH RAMGARHIA <81849508+JasvinderSingh131198@users.noreply.github.com>
2026-09-19 18:20:06 +0530

    Restore commented-out code in FirstTypescriptExample.ts

--- Typescript Programs/basics/FirstTypescriptExample.ts
+++ Typescript Programs/basics/FirstTypescriptExample.ts
@@ -15,14 +15,3 @@ let sum: number = firstNumber + secondNumber;
 console.log('Sum of two numbers is:', sum);
 
 
-//Ask : When we have to ask questions and I want answer from AI that time ask option we use.
-//Agent : When we need to perform some action and I want action from AI that time we use agent option.
-//Plan : we should not use plamn option
-
-
-//Prompt Engineering
-//Tokens in Any AI Model
-//- Tokens are the pieces of words that the model uses to understand and generate text. 
-// For example, the word "Hello" is one token, while "Hello, world!" is two tokens. 
-// The number of tokens affects how much text can be processed at once and can influence 
-// the cost of using AI models.
